4e64b84a3276e42d13886624757a4af63d40dc11
Patrick Bell <patrick@Patricks-Laptop.local>
2024-09-20 00:24:49 +0100

    update

--- backend/server.js
+++ backend/server.js
@@ -145,11 +145,7 @@ cron.schedule('0 0 * * * ', async () => {
 })
 
 
-
-
-
-
-
-app.listen(3000, () => {
-    console.log('App listening on port 3000')
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+});
